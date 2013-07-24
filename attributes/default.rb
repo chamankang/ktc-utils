7826968194a8d795d04479b83bbd2311f84d9697
Wil Reichert <wil.reichert@kt.com>
2013-07-24 23:47:12 +0900

    move interface mapping attrivutes to the environment

--- attributes/default.rb
+++
@@ -1,6 +0,0 @@
-
-default["interface_mapping"]["management"] = "eth1"
-default["interface_mapping"]["private"] = "eth2"
-default["interface_mapping"]["disk"] = "eth3"
-default["interface_mapping"]["public"] = "eth4"
-
