2dc3c9097326d68a4ebbe2ad5f510016f047954c
Wil Reichert <wil.reichert@kt.com>
2013-07-25 01:15:56 +0900

    revert

---
+++ attributes/default.rb
@@ -0,0 +1,5 @@
+
+default["interface_mapping"]["management"] = "eth1"
+default["interface_mapping"]["disk"] = "eth2"
+default["interface_mapping"][private"] = "eth3"
+default["interface_mapping"]["public"] = "eth4"
