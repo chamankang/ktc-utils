0abd9d6743168666dbc9406c85f6c945833e0d0e
Wil Reichert <wil.reichert@kt.com>
2013-08-23 13:57:55 +0900

    cnode iterface differences

--- attributes/default.rb
+++ attributes/default.rb
@@ -6,5 +6,11 @@ when "dev"
   default["interface_mapping"]["private"] = "eth3"
   default["interface_mapping"]["public"] = "eth4"
 when "mkd_stag"
-  default["interface_mapping"]["management"] = "eth0"
+  if node.run_list.include?("recipe[ktc-compute]")
+    default["interface_mapping"]["service"] = "eth0"
+    default["interface_mapping"]["management"] = "eth1"
+    default["interface_mapping"]["storage"] = "eth2"
+  else
+    default["interface_mapping"]["management"] = "eth0"
+  end
 end
