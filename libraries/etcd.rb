b7f6edefc5ea80674836e59282ca4848d8a748bd
Wil Reichert <wil.reichert@kt.com>
2013-08-26 19:37:29 +0900

    dont add empyt service defs

--- libraries/etcd.rb
+++ libraries/etcd.rb
@@ -67,10 +67,16 @@ module KTCUtils
     data = Hash.new
     node = base["key"].split("/").last
     ep = client.get(base["key"])
-    ep.each do |k|
-      data[k["key"].split("/").last] = k.value
+    # if no keys exist underneath this service don't add anything
+    # as it doesn't seem like directories can be deleted =/
+    unless ep.empty?
+      ep.each do |k|
+        data[k["key"].split("/").last] = k.value
+      end
+      return { node=>data }
+    else
+      return {}
     end
-    return { node=>data }
   end
 
   # obtain service information from etcd
