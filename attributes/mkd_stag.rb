ae2782b3c670b3c0f04580d42f795a671a238178
Chaman <chaman.kang@kt.com>
2013-08-26 21:27:08 +0900

    set interface_mapping on cnodes

--- attributes/mkd_stag.rb
+++ attributes/mkd_stag.rb
@@ -1,6 +1,6 @@
 return unless chef_environment == "mkd_stag"
 
-if node.run_list.include?("recipe[ktc-compute]")
+if node.run_list.include?("recipe[ktc-compute::compute]")
   default["interface_mapping"]["private"] = "eth0"
   default["interface_mapping"]["management"] = "eth1"
   default["interface_mapping"]["storage"] = "eth2"
