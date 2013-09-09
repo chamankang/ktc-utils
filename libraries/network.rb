52893e57b04dfc652176cb826c286979a3723db9
Jesse Nelson <spheromak@gmail.com>
2013-09-09 10:24:45 +0900

    add last_octet method
    returns the last octet of a ip address. used in building keepalived
    routers

--- libraries/network.rb
+++ libraries/network.rb
@@ -6,23 +6,26 @@ module KTC
 
       # get the interface associated with a interface name
       # @param String interface name
-      # @return String eth name
+      # @return String eth name or nil
       def if_lookup name
         if list_interface_names.include?(name)
-          return node.default["interface_mapping"][name]
+          return node["interface_mapping"][name]
         end
+        nil
       end
 
       # get the ip associated with an interface name
-      # @param String interface name
+      # @param String interface name: private/management or ethX
       # @return String ip address
       def address name
         ip = nil
-        iface = get_interface name
-        if list_interfaces.include?(iface)
-          ip =  if_addr iface
-        end
-        ip
+        iface = if_lookup name || name
+        ip =  if_addr iface
+      end
+
+      # return last octet of the given ipaddr
+      def last_octet ipaddr
+        ipaddr.split(".")[3].to_i
       end
 
       private
@@ -39,13 +42,13 @@ module KTC
       # list all the defined interace names
       # @return Array of names
       def list_interface_names
-        return node.default["interface_mapping"].keys
+        return node["interface_mapping"].keys
       end
 
       # list interfaces on this vm
       # @return Array of eth names
       def list_interfaces
-        return node.automatic["network"]["interfaces"].keys
+        return node["network"]["interfaces"].keys
       end
 
     end
