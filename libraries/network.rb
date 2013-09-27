b86f9e26c38fe5eb42c57c37cfbac1ba7b98b7f5
Wil Reichert <wil.reichert@kt.com>
2013-09-27 21:24:12 +0900

    make public

--- libraries/network.rb
+++ libraries/network.rb
@@ -28,29 +28,6 @@ module KTC
         ipaddr.split(".")[3].to_i
       end
 
-      private
-
-      def if_addr int
-        interface_node = node["network"]["interfaces"][int]["addresses"]
-        interface_node.select do |address, data|
-          if data['family'] == "inet"
-            return address
-          end
-        end
-      end
-
-      # list all the defined interace names
-      # @return Array of names
-      def list_interface_names
-        return node["interface_mapping"].keys
-      end
-
-      # list interfaces on this vm
-      # @return Array of eth names
-      def list_interfaces
-        return node["network"]["interfaces"].keys
-      end
-
       def add_service_nat service_name, port
         #node.run_context.include_recipe "simple_iptables::default"
         ep = Services::Endpoint.new service_name
@@ -79,6 +56,30 @@ module KTC
         #  jump false
         #end
       end
+
+      private
+
+      def if_addr int
+        interface_node = node["network"]["interfaces"][int]["addresses"]
+        interface_node.select do |address, data|
+          if data['family'] == "inet"
+            return address
+          end
+        end
+      end
+
+      # list all the defined interace names
+      # @return Array of names
+      def list_interface_names
+        return node["interface_mapping"].keys
+      end
+
+      # list interfaces on this vm
+      # @return Array of eth names
+      def list_interfaces
+        return node["network"]["interfaces"].keys
+      end
+
     end
   end
 end
