60e0f47710a36c7ae1e4600e63b949e0ef0ad63a
Jesse Nelson <spheromak@gmail.com>
2014-01-24 12:30:38 -0800

    add ipc-stage conditionals for mnode/zonode
    may need other conditions or to revise the rest.

--- attributes/ipc-stage.rb
+++ attributes/ipc-stage.rb
@@ -7,11 +7,22 @@ if node.run_list.include?("recipe[ktc-compute::compute]")
   default["interface_mapping"]["management"] = "eth1"
   default["interface_mapping"]["storage"] = "eth0"
 end
+
 if node.run_list.include?("recipe[ktc-image]")
   default["interface_mapping"]["management"] = "eth1"
   default["interface_mapping"]["storage"] = "eth0"
 end
+
 if node.run_list.include?("recipe[ktc-block-storage]")
   default["interface_mapping"]["management"] = "eth0"
   default["interface_mapping"]["storage"] = "eth1"
+end
+
+if node.hostname.include?("znode")
+    default["interface_mapping"]["management"] = "br0"
+      default["interface_mapping"]["storage"] = "br1"
+end
+
+if node.hostname.include?("mnode")
+    default["interface_mapping"]["management"] = "br0"
 end
