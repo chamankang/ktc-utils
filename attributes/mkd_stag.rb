c39bae0e9da599a8b2039d1ea23718aeaa405ffc
chamankang <chaman.kang@nexr.com>
2014-03-07 15:54:29 +0900

    fix mkd_stag interfaces

--- attributes/mkd_stag.rb
+++ attributes/mkd_stag.rb
@@ -4,7 +4,7 @@ return unless chef_environment == 'mkd_stag'
 include_attribute 'ktc-utils::default'
 
 if node.run_list.include?('recipe[ktc-compute::compute]')
-  default['interface_mapping']['private'] = 'eth0'
+  default['interface_mapping']['private'] = 'eth2'
   default['interface_mapping']['management'] = 'eth1'
-  default['interface_mapping']['storage'] = 'eth2'
+  default['interface_mapping']['storage'] = 'eth0'
 end
