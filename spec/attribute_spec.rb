fce9a9eec8b3855f0d7e14837c530fb1f5f511ac
chamankang <chaman.kang@kt.com>
2014-04-29 16:34:26 +0900

    add attributes library spec

---
+++ spec/attribute_spec.rb
@@ -0,0 +1,29 @@
+require_relative 'spec_helper'
+require_relative '../libraries/attributes'
+
+describe 'ktc-utils::Attributes' do
+  let(:service) {
+    {
+      service: 'utils-test-service',
+      port: 55555,
+      proto: 'xxx',
+      ip: '127.0.0.1'
+    }
+  }
+  before do
+    runner.converge "etcd::compile_time"
+    Services::Connection.new host: '127.0.0.1'
+    utils_test_service = Services::Member.new 'localhost.localdomain', service
+    utils_test_service.save
+
+  end
+  context 'get_endpoint' do
+
+    it 'should lookup the right service' do
+      utils_test_service = Services::Service.new 'utils-test-service'
+      ha_disabled = true
+      KTC::Attributes.get_endpoint(utils_test_service, ha_disabled).should ==
+        [service['ip'], service['port']]
+    end
+  end
+end
